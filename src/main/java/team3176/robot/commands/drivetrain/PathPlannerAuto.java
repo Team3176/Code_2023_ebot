507a326f58a054154f925a00b17e97e02406f6ba
char@odin <char3176@gmail.com>
2023-07-05 14:15:22 -0400

    Added delays to center autos

--- src/main/java/team3176/robot/commands/drivetrain/PathPlannerAuto.java
+++ src/main/java/team3176/robot/commands/drivetrain/PathPlannerAuto.java
@@ -10,6 +10,7 @@ import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import team3176.robot.commands.superstructure.intakecube.PoopCube;
 import team3176.robot.constants.DrivetrainConstants;
 import team3176.robot.subsystems.drivetrain.Drivetrain;
@@ -38,6 +39,7 @@ public class PathPlannerAuto {
         eventMap.put("CubeAlignAtPoint", new CubeAlignAtPoint());
         eventMap.put("CubeChaseOn", driveSubsystem.setCubeChaseOn());
         eventMap.put("CubeChaseOff", driveSubsystem.setCubeChaseOff());
+        eventMap.put("delay", new WaitCommand(10.0));
         // eventMap.put("intakeDown", new IntakeDown());
         // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
         SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
