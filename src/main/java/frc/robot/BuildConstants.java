b29b27279fd0fd2aa0c16487694cc0b7e366f338
Jonathan Heidegger <jth3141@gmail.com>
2023-11-09 19:26:24 -0500

    build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2023_ebot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 331;
-  public static final String GIT_SHA = "2b48c672cb30140570b757b88b33e2dcea3361b1";
-  public static final String GIT_DATE = "2023-11-09 11:42:52 EST";
+  public static final int GIT_REVISION = 332;
+  public static final String GIT_SHA = "9b867d761c0dc301a185ea842e5f72b59f141f7a";
+  public static final String GIT_DATE = "2023-11-09 12:01:37 EST";
   public static final String GIT_BRANCH = "2024_beta";
-  public static final String BUILD_DATE = "2023-11-09 11:56:58 EST";
-  public static final long BUILD_UNIX_TIME = 1699549018153L;
+  public static final String BUILD_DATE = "2023-11-09 19:22:37 EST";
+  public static final long BUILD_UNIX_TIME = 1699575757581L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
